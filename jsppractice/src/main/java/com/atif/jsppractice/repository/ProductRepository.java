729f0c7698920226e2ae06e03efc5f8de5af5c0f
Atif <khan.atif858@gmail.com>
2023-02-21 18:52:17 +0530

    Changed the order of query in ProductRepository

--- jsppractice/src/main/java/com/atif/jsppractice/repository/ProductRepository.java
+++ jsppractice/src/main/java/com/atif/jsppractice/repository/ProductRepository.java
@@ -14,7 +14,7 @@ public interface ProductRepository extends CrudRepository<ProductEntity, Integer
 	@Query("SELECT p FROM ProductEntity p ORDER BY purchase_timestamp desc")
 	public List<ProductEntity> getProductPurchaseInDescOrderByDate();
 	
-	@Query("SELECT p FROM ProductEntity p WHERE productId = :productId ORDER BY purchase_timestamp, size LIMIT 1")
+	@Query("SELECT p FROM ProductEntity p WHERE productId = :productId ORDER BY purchase_timestamp DESC, size LIMIT 1")
 	public ProductEntity findProductByProductId(@Param("productId") String productId);
 	
 //	@Query(value="SELECT * FROM Product_purchase GROUP BY product_id", nativeQuery=true) This also works
